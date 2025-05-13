2f6ff118374f877028fcccdb1f3be91a07ad4a1b
Dani <drs35@alu.ua.es>
2025-05-13 12:08:23 +0200

    Exportar factura a XML terminado

--- service/DocumentoService.js
+++ service/DocumentoService.js
@@ -2,8 +2,20 @@
 var DB = require('../ConexionDB/Conexion');
 var DBFactura = require('../ConexionDB/FacturaRepository');
 var SMTP = require('../ConexionFakeSMTP/ConexionFakeSMTP');
+const fs        = require('fs');
+const path      = require('path');
+const xml2js    = require('xml2js');
 
-
+function formatTimestamp(date) {
+  const pad = n => String(n).padStart(2, '0');
+  const Y = date.getFullYear();
+  const M = pad(date.getMonth() + 1);
+  const D = pad(date.getDate());
+  const h = pad(date.getHours());
+  const m = pad(date.getMinutes());
+  const s = pad(date.getSeconds());
+  return `${Y}${M}${D}_${h}${m}${s}`;
+}
 
 async function validarYObtenerFactura(wSKey, numeroFactura, emailEmpresa) {
   const restParam = await DB.obtenerRestKey();
@@ -37,7 +49,7 @@ exports.documentoUtilityExportarFacturaPDF = function(wSKey,numeroFactura,emailE
     try {
       const factura = await validarYObtenerFactura(wSKey, numeroFactura, emailEmpresa);
       console.log(factura);
-      
+
       resolve({ mensaje: { mensaje: 'Operación realizada con éxito' }, status: 200 });
     } catch (err) {
       reject(manejarError(err));
@@ -59,6 +71,17 @@ exports.documentoUtilityExportarFacturaXML = function(wSKey,numeroFactura,emailE
      try {
       const factura = await validarYObtenerFactura(wSKey, numeroFactura, emailEmpresa);
       console.log(factura);
+
+      const builder = new xml2js.Builder({ headless: true, renderOpts: { pretty: true } });
+      const xmlObj = { Factura: factura };
+      const xml     = builder.buildObject(xmlObj);
+
+      const timestamp = formatTimestamp(new Date());
+      const filename  = `${numeroFactura}_${timestamp}.xml`;
+      const filepath  = path.join(__dirname, '../exported_xml', filename);
+
+      await fs.promises.mkdir(path.dirname(filepath), { recursive: true });
+      await fs.promises.writeFile(filepath, xml, 'utf8');
 
       resolve({ mensaje: { mensaje: 'Operación realizada con éxito' }, status: 200 });
     } catch (err) {
